93049185014175200956c84bbbd0cc7762673810
Gabriele Petronella <gabriele@buildo.io>
2016-06-13 19:03:47 +0200

    Add ignoreTrailingComments to max-len

--- rules/default.js
+++ rules/default.js
@@ -38,7 +38,7 @@ module.exports = {
 
     // Stylistic Issues
     "operator-assignment": [2, "never"],
-    "max-len": ["error", 100],
+    "max-len": ["error", 100, { "ignoreTrailingComments": true }],
     "no-plusplus": 2,
     "semi": [2, "always"],
     "no-trailing-spaces": 2,
