b25c94690269d7cefa235de5d84d26bc82fe6d88
Francesco <firefelix@gmail.com>
2016-11-10 11:43:35 +0100

    fix deprecation: rename react/wrap-multilines to react/jsx-wrap-multilines

--- rules/default.js
+++ rules/default.js
@@ -104,7 +104,7 @@ module.exports = {
     "react/prefer-es6-class": 2,
     "react/react-in-jsx-scope": 2,
     "react/self-closing-comp": 2,
-    "react/wrap-multilines": 2,
+    "react/jsx-wrap-multilines": 2,
 
     // Warnings
     // things that could be ok in dev but not in prod
